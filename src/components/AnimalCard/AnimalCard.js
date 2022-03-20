6fc9a2349a2319a432c9b2a34a2c53e67489210c
kamilciecierski <kamil.ciecierski99@gmail.com>
2022-03-20 21:47:49 +0100

    first commit

---
+++ src/components/AnimalCard/AnimalCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import Card from "../Card/Card";
+import AnimalDetails from "../AnimalDetails/AnimalDetails";
+
+export default function AnimalCard({name, size, ...props})
+{
+    return(
+        <div>
+            <Card title="Animal" 
+            details={
+                <AnimalDetails
+                    {...props}
+                />
+                }>
+                <h3>{name}</h3>
+                <div>{size}kg</div>
+                
+            </Card>
+        </div>
+    )
+}
+
+AnimalCard.propTypes = {
+    name: PropTypes.string.isRequired,
+    size: PropTypes.number.isRequired,
+}
